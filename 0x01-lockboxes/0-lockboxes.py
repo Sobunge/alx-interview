e0c01d2db380ddbbcf235722dea12e2d81b89a51
Samuel Obunge <samuelobunge@yahoo.com>
2024-07-03 11:08:21 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-from typing import List
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+    :param boxes: List of lists where each sublist
+                  represents a box and the integers
+                  in each sublist represent the keys contained in that box.
+    :return: True if all boxes can be opened, else False.
+    """
+    if not boxes:
+        return False
 
-def canUnlockAll(boxes: List[List[int]]) -> bool:
     n = len(boxes)
-    unlocked = set()
-    stack = [0]  # Start with the first box
-    
-    while stack:
-        box = stack.pop()
-        if box not in unlocked:
-            unlocked.add(box)
-            for key in boxes[box]:
-                if key < n:  # Ensure the key corresponds to a valid box
-                    stack.append(key)
-    
-    return len(unlocked) == n
+    opened = [False] * n
+    opened[0] = True  # The first box is always opened
+    keys = [0]  # Start with the key to the first box
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
+
+
+# Example usage
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
