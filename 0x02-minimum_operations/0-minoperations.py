f2195ef18b837dbffd64727236bdeb527f2d94f0
Samuel Obunge <samuelobunge@yahoo.com>
2024-07-09 15:39:59 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,18 +14,15 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
     return operations
-
 # Main file for testing
 if __name__ == "__main__":
     n = 4
