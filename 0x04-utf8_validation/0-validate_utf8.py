88a3808c18fab0eae3d9fbef073e8eceba94f89c
Samuel Obunge <samuelobunge@yahoo.com>
2024-07-23 11:10:35 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,49 +6,34 @@ def validUTF8(data):
     """Determines if a given data set
     represents a valid utf-8 encoding
     """
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
+    number_bytes = 0
 
-    # Masks to check the most significant bits
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-    # Iterate over each integer in the data list
-    for num in data:
-        # Get the binary representation of the integer
-        # and keep only the 8 least significant bits
-        bin_rep = format(num, '#010b')[-8:]
+    for i in data:
 
-        # If this is the start of a new UTF-8 character
-        if n_bytes == 0:
-            # Determine the number of bytes in the character
-            for i in range(8):
-                if (num & (mask1 >> i)) == 0:
-                    break
-                n_bytes += 1
+        mask_byte = 1 << 7
 
-            # 1-byte character
-            if n_bytes == 0:
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
                 continue
 
-            # Invalid scenarios according to UTF-8 rules
-            if n_bytes == 1 or n_bytes > 4:
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            # If this is a continuation byte, it must start with '10'
-            if not (num & mask1 and not (num & mask2)):
+            if not (i & mask_1 and not (i & mask_2)):
                 return False
 
-        # Decrement the number of bytes left for the current character
-        n_bytes -= 1
+        number_bytes -= 1
 
-    # All characters must be completely processed
-    return n_bytes == 0
+    if number_bytes == 0:
+        return True
 
-
-# Example usage:
-data = [197, 130, 1]  # Valid UTF-8 encoding
-print(validUTF8(data))  # Output: True
-
-data = [235, 140, 4]  # Invalid UTF-8 encoding
-print(validUTF8(data))  # Output: False
+    return False
